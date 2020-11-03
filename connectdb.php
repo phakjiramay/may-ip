1261710d980cc4a547babbea310293a5bab04fb7
phakjiramay <std60010912592@acc.msu.ac.th>
2020-11-04 03:20:07 +0700

    h

--- connectdb.php
+++ connectdb.php
@@ -20,4 +20,4 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-mysqli_set_charset($con,"utf8");
+mysqli_set_charset($conn,"utf8");
