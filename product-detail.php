11841a4257e9cacae1d07d9750f049193efed7a3
phakjiramay <std60010912592@acc.msu.ac.th>
2020-11-04 04:35:55 +0700

    y

--- product-detail.php
+++ product-detail.php
@@ -110,9 +110,8 @@
         </div>
         <?php 
 						include_once("connectdb.php");
-						$sql = "SELECT * FROM `pdfemale`";
+						$sql = "SELECT * FROM `pdfemale` WHERE p_id='{$_GET["p_id"]}'";
 						$result = mysqli_query($conn, $sql);
-
 						if (mysqli_num_rows($result) > 0) {
 						// output data of each row
 							while($row = mysqli_fetch_assoc($result)) { 
