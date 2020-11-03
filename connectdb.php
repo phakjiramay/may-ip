6c43d2b92b8889386aa0c36fa79e58ee05d0e73d
phakjiramay <std60010912592@acc.msu.ac.th>
2020-11-04 03:20:30 +0700

    g

--- connectdb.php
+++ connectdb.php
@@ -18,6 +18,6 @@ $dbname = "shoesshop";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+  die("Connection failed: "  . mysqli_connect_error());
 }
 mysqli_set_charset($conn,"utf8");
