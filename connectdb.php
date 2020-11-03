2f1a13a26dfe093901315d3aa71965b5deae20af
phakjiramay <std60010912592@acc.msu.ac.th>
2020-11-04 03:15:39 +0700

    h

--- connectdb.php
+++ connectdb.php
@@ -1,11 +1,22 @@
-<meta charset="utf-8">
-<?php
-$host = "localhost";
-$usr = "root";
-$pwd = "";
-$dbName = "shoesshop";
+ <?php
+// $host = "localhost";
+// $usr = "root";
+// $pwd = "";
+// $dbName = "shoesshop";
 
-$conn = mysqli_connect($host, $usr, $pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้") ;
-mysqli_select_db($conn, $dbName) ;
-mysqli_query($conn, "set names utf8");
-?>
+// $conn = mysqli_connect($host, $usr, $pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้") ;
+// mysqli_select_db($conn, $dbName) ;
+// mysqli_query($conn, "set names utf8");
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shoesshop";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
