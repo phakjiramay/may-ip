3b3a6202c921647dc9c41a04726f4ef8f054b903
phakjiramay <std60010912592@acc.msu.ac.th>
2020-11-04 14:56:08 +0700

    i

--- clear.php
+++ clear.php
@@ -4,7 +4,7 @@
 	session_destroy();
 
 	echo "กำลังกลับหน้าหลัก กรุณารอสักครู่....";
-	echo "<meta http-equiv=\"refresh\" content=\"2;URL=index2.php\">";
+	echo "<meta http-equiv=\"refresh\" content=\"2;URL=index.php\">";
 	//header("Location: index.php");
 
 ?>
